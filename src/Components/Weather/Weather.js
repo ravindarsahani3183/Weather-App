8bd3f6707e849dda3d5d6ed36bb05699f40d3c2f
ravindarsahani3183 <ravindarsahani3183@gmail.com>
2024-01-28 20:49:16 +0530

    add image and search icon

--- src/Components/Weather/Weather.js
+++ src/Components/Weather/Weather.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import "./Weather.css";
+import search_icon from "../Assets/search.jpeg"
+import cloud_icon from "../Assets/cloud.png"
 const Weather = () => {
   return (
     <div className='container'>
         <div className='top-bar'>
             <input type='text' className='cityInput' placeholder='Search'>
             </input>
-            <div className='search-icon'>
-
+            <div className=''>
+              <img src={search_icon} className='search-icon' alt=''></img>
             </div>
+        </div>
+        <div className='weather-image'>
+          <img src={cloud_icon}  alt=''></img>
+        </div>
+        <div className='weather-temp'>24℃</div>
+        <div className='weather-location'>Mumbai</div>
+        <div className='data-container'>
+          <div className='element'>
+            <img className='icon'></img>
+            <div className='data'></div>
+          </div>
         </div>
     </div>
   )
